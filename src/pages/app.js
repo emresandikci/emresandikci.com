8f088f60a1637133fb8b75ac056631d5e4443040
emresandikci <me@emresandikci.com>
2020-05-04 01:38:03 +0300

    feat: home page completed

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Box } from 'esducad-ui';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { defaultTheme, GlobalStyle } from 'esducad-ui/dist/utils';
 import { Home } from 'pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Seo } from 'utils';
 
+const CustomStyle = createGlobalStyle`
+  body{
+    font-family: 'Zilla Slab Highlight', cursive;
+    background-color:#42E6A4;
+    line-height:1.2;
+  }
+`;
+
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <CustomStyle />
       <Seo />
-      <Box>
-        <Router>
-          <Switch>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
-      </Box>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Box, Heading, Text, Button } from 'esducad-ui';
+import { Box } from 'esducad-ui';
+import { SocialIcons, Logo, Description, HireMe } from 'components';
+
 function Home() {
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      height="100vh"
-      justifyContent="center"
-      alignItems="center"
-      bg="primary"
-    >
-      <Heading fontSize="50px" color="white" animation="bounce">
-        React easy starter boilerplate
-      </Heading>
-      <Text color="white" animation="bounce">
-        by emresandikci to see the other projects{' '}
-        <Button
-          variant="secondary"
-          onClick={() => (window.location.href = 'http://github.com/emresandikci')}
-        >
-          click here
-        </Button>
-      </Text>
+    <Box display="flex" flexDirection="column" justifyContent="space-around" height="100vh">
+      <Logo />
+      <Description />
+      <SocialIcons />
+      <HireMe />
     </Box>
   );
 }
