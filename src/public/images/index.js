38141b5556e08668dbf9222f045c128c564704d6
emresandikci <me@emresandikci.com>
2020-05-04 01:38:45 +0300

    feat: static public files added

--- src/public/images/index.js
+++ src/public/images/index.js
@@ -1,1 +1,5 @@
 export { default as logo } from './Logo.svg';
+export { default as github } from './github.svg';
+export { default as linkedin } from './linkedin.svg';
+export { default as medium } from './medium.svg';
+export { default as twitter } from './twitter.svg';
