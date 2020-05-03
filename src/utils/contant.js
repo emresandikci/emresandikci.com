09bb7d0e4c412c78d6ff92c9dd0a08e46661de5d
emresandikci <me@emresandikci.com>
2020-05-04 01:37:16 +0300

    feat: constant added

--- src/utils/contant.js
+++ src/utils/contant.js
@@ -1,11 +1,13 @@
 const constant = {
   seo: {
-    title: 'React boilerplate',
-    description: '',
-    baseUrl: 'http://www.emresandikci.com',
+    title: 'Full-stack Developer',
+    description:
+      'Emre is highly qualified in back-end and front-end techs for more than 6+ years.He is curious, researcher, problem solver, optimist and empathetic, a good teammate, also helpful person. ',
+    baseUrl: 'https://www.emresandikci.com',
     ogTitle: 'Full Stack Developer',
-    ogDescription: 'JS,Reactjs,Nodejs...',
-    ogUrl: '',
+    ogDescription:
+      'Emre is highly qualified in back-end and front-end techs for more than 6+ years.He is curious, researcher, problem solver, optimist and empathetic, a good teammate, also helpful person.',
+    ogUrl: 'https://www.emresandikci.com',
     ogImage: '',
     ogImageAltTag: '',
     ogMediaType: '',
@@ -19,6 +21,13 @@ const constant = {
       website: 'website',
       article: 'article',
     },
+  },
+  contact: {
+    linkedIn: 'https://www.linkedin.com/in/emresandikci/',
+    github: 'https://github.com/emresandikci',
+    medium: 'https://medium.com/@emresandikci',
+    twitter: 'https://twitter.com/emresand1kc1',
+    email: 'me@emresandikci.com',
   },
 };
 
